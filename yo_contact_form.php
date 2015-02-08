d4ab336566ae837c51a0d6b506fc0af0ee831cb1
Gurjyot <gurjyottheman@gmail.com>
2015-02-09 00:37:27 +0530

    second commit

---
+++ yo_contact_form.php
@@ -0,0 +1,8 @@
+<form action="yo_cont.php" method="POST" >
+Name: <input type="text" name="name"><br/><br/> 
+Email: <input type="email" name="email"><br/><br/> 
+Phone Number: <input type="text" name="phone_number"><br/><br/>
+Website: <input type="text" name="website"><br/><br/>
+Message: <textarea name="message" rows="6" cols="25"></textarea><br/><br/> 
+<input type="submit" name="submit" value="Submit">
+</form>
